651e8d45803ab5bbb481e38ed196f6d1c943f0a7
schmeller <eduardoschmeller@hotmail.com>
2023-07-07 11:27:42 -0300

    initial commit

---
+++ backend/src/controllers/tasksController.js
@@ -0,0 +1,14 @@
+const { response } = require('express');
+const tasksModel = require ('../models/taskModel.js');
+
+
+const getAll = async (req, res) => {
+
+  const tasks = await tasksModel.getAll();
+  return res.status(200).json({tasks});
+  
+};
+
+module.exports = {
+  getAll
+};
